8fdbc97726d992a9fc30f3e3fe66f328b34995c6
Vaishnavi Panday <pandayvaishnavi459@gmail.com>
2026-07-02 21:06:16 +0530

    resolve import error

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { User, Mail, Lock, Camera, ArrowRight } from "lucide-react";
